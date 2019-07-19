4273bc20b8e4eff742848554911c0141a376dad8
Jozef Kralik <jojo.lwin@gmail.com>
2019-07-19 16:49:10 +0200

    add support for intermediate certs

--- cmd/certificate-generator/main.go
+++ cmd/certificate-generator/main.go
@@ -10,23 +10,31 @@ import (
 	"encoding/pem"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math/big"
 	"net"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/go-ocf/kit/security"
 )
 
 var (
-	host             = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
-	validFor         = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
-	generateCA       = flag.Bool("generateCa", false, "whether this cert should be its own Certificate Authority")
-	ecdsaCurve       = flag.String("ecdsa-curve", "P256", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
-	generateIdentity = flag.String("generateIdentity", "", "uuid of device")
-	caKey            = flag.String("caKey", "", "caKey")
-	caCert           = flag.String("caCert", "", "caPem")
+	host                   = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
+	validFor               = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	generateCA             = flag.Bool("generateCa", false, "whether this cert should be its own Certificate Authority")
+	ecdsaCurve             = flag.String("ecdsa-curve", "P256", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
+	generateIdentity       = flag.String("generateIdentity", "", "uuid of device")
+	caKey                  = flag.String("caKey", "", "caKey")
+	caCert                 = flag.String("caCert", "", "caPem")
+	outCert                = flag.String("outCert", "", "certificate out file")
+	outKey                 = flag.String("outKey", "", "private key out file")
+	generateIntermediateCA = flag.Bool("generateIntermediateCA", false, "whether this cert should be its Intermediate Certificate Authority signed by Certificate Authority")
+
+	subjectOrganization = flag.String("subjectOrganization", "", "subject organization")
+	subjectCountry      = flag.String("subjectCountry", "", "subject country")
+	subjectCommonName   = flag.String("subjectCommonName", "", "subject country")
 )
 
 var ekuOcfId = asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 44924, 1, 6}
@@ -66,8 +74,8 @@ func main() {
 	if *generateIdentity != "" && len(*caCert) == 0 {
 		log.Fatalf("Missing required --caCert parameter")
 	}
-	if !*generateCA && *generateIdentity == "" {
-		log.Fatalf("Missing required --generateCa or --generateIdentity  parameter")
+	if !*generateCA && *generateIdentity == "" && !*generateIntermediateCA {
+		log.Fatalf("Missing required --generateCa or --generateIdentity or --generateIntermediateCA parameter")
 	}
 
 	var priv interface{}
@@ -102,13 +110,19 @@ func main() {
 	template := x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			Organization: []string{"Test ORG"},
+			CommonName: *subjectCommonName,
 		},
 		NotBefore:             notBefore,
 		NotAfter:              notAfter,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		UnknownExtKeyUsage:    []asn1.ObjectIdentifier{},
 		BasicConstraintsValid: true,
+	}
+	if *subjectOrganization != "" {
+		template.Subject.OrganizationalUnit = append(template.Subject.OrganizationalUnit, *subjectOrganization)
+	}
+	if *subjectOrganization != "" {
+		template.Subject.Country = append(template.Subject.Country, *subjectCountry)
 	}
 
 	hosts := strings.Split(*host, ",")
@@ -124,55 +138,71 @@ func main() {
 
 	var parent *x509.Certificate
 	var privateKeyOfSigner interface{}
-	certFilename := "cert.pem"
-	keyFilename := "key.pem"
-
 	if *generateIdentity != "" {
+		if *outCert == "" {
+			*outCert = "cert.pem"
+		}
+		if *outKey == "" {
+			*outKey = "key.pem"
+		}
 		template.KeyUsage |= x509.KeyUsageDigitalSignature | x509.KeyUsageKeyAgreement
 		template.Subject.CommonName = fmt.Sprintf("uuid:%v", *generateIdentity)
 		template.UnknownExtKeyUsage = append(template.UnknownExtKeyUsage, ekuOcfId)
 		template.ExtKeyUsage = append(template.ExtKeyUsage, x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth)
 
-		certPEMBlock, err := ioutil.ReadFile(*caCert)
+		cert, err := security.LoadX509(*caCert)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "cannot load ca certificate for signing: %v", err)
 			os.Exit(1)
 		}
-		keyPEMBlock, err := ioutil.ReadFile(*caKey)
-		if err != nil {
+		key, e := security.LoadX509PrivateKey(*caKey)
+		if e != nil {
 			fmt.Fprintf(os.Stderr, "cannot load ca key for signing: %v", err)
 			os.Exit(1)
 		}
-		certBlock, _ := pem.Decode(certPEMBlock)
-		if certBlock == nil {
-			fmt.Fprintf(os.Stderr, "cannot decode pem from ca pem for signing")
-			os.Exit(1)
-		}
-		cert, err := x509.ParseCertificate(certBlock.Bytes)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cannot parse ca cert for signing: %v", err)
-			os.Exit(1)
-		}
-		keyBlock, _ := pem.Decode(keyPEMBlock)
-		if keyBlock == nil {
-			fmt.Fprintf(os.Stderr, "cannot decode pem from ca pem for signing")
-			os.Exit(1)
-		}
-		key, e := x509.ParseECPrivateKey(keyBlock.Bytes)
-		if e != nil {
-			fmt.Fprintf(os.Stderr, "cannot parse ca key for signing: %v", err)
-			os.Exit(1)
-		}
-
-		parent = cert
+		parent = cert[0]
 		privateKeyOfSigner = key
 	} else if *generateCA {
-		certFilename = "cacert.pem"
-		keyFilename = "cakey.pem"
+		if *subjectCommonName == "" {
+			fmt.Fprintf(os.Stderr, "subjectCommonName cannot be empty: %q", *ecdsaCurve)
+			os.Exit(1)
+		}
+		if *outCert == "" {
+			*outCert = "cacert.pem"
+		}
+		if *outKey == "" {
+			*outKey = "cakey.pem"
+		}
 		template.IsCA = true
 		template.KeyUsage |= x509.KeyUsageCertSign | x509.KeyUsageCRLSign
 		parent = &template
 		privateKeyOfSigner = priv
+	} else if *generateIntermediateCA {
+		if *subjectCommonName == "" {
+			fmt.Fprintf(os.Stderr, "subjectCommonName cannot be empty: %q", *ecdsaCurve)
+			os.Exit(1)
+		}
+		if *outCert == "" {
+			*outCert = "intermediatecacert.pem"
+		}
+		if *outKey == "" {
+			*outKey = "intermediatecakey.pem"
+		}
+		template.IsCA = true
+		template.KeyUsage |= x509.KeyUsageCertSign | x509.KeyUsageCRLSign
+
+		cert, err := security.LoadX509(*caCert)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cannot load ca certificate for signing: %v", err)
+			os.Exit(1)
+		}
+		key, e := security.LoadX509PrivateKey(*caKey)
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "cannot load ca key for signing: %v", err)
+			os.Exit(1)
+		}
+		parent = cert[0]
+		privateKeyOfSigner = key
 	}
 
 	derBytes, err := x509.CreateCertificate(rand.Reader, &template, parent, publicKey(priv), privateKeyOfSigner)
@@ -180,28 +210,28 @@ func main() {
 		log.Fatalf("Failed to create certificate: %s", err)
 	}
 
-	certOut, err := os.Create(certFilename)
+	certOut, err := os.Create(*outCert)
 	if err != nil {
-		log.Fatalf("failed to open %v for writing: %s", certFilename, err)
+		log.Fatalf("failed to open %v for writing: %s", *outCert, err)
 	}
 	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
-		log.Fatalf("failed to write data to %v: %s", certFilename, err)
+		log.Fatalf("failed to write data to %v: %s", *outCert, err)
 	}
 	if err := certOut.Close(); err != nil {
-		log.Fatalf("error closing %v: %s", certFilename, err)
+		log.Fatalf("error closing %v: %s", *outCert, err)
 	}
 
-	log.Printf("wrote %v\n", certFilename)
+	log.Printf("wrote %v\n", *outCert)
 
-	keyOut, err := os.OpenFile(keyFilename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	keyOut, err := os.OpenFile(*outKey, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("failed to open %v for writing: %v", keyFilename, err)
+		log.Fatalf("failed to open %v for writing: %v", *outKey, err)
 	}
 	if err := pem.Encode(keyOut, pemBlockForKey(priv)); err != nil {
-		log.Fatalf("failed to write data to %v: %s", keyFilename, err)
+		log.Fatalf("failed to write data to %v: %s", *outKey, err)
 	}
 	if err := keyOut.Close(); err != nil {
-		log.Fatalf("error closing %v: %s", keyFilename, err)
+		log.Fatalf("error closing %v: %s", *outKey, err)
 	}
-	log.Printf("wrote %v\n", keyFilename)
+	log.Printf("wrote %v\n", *outKey)
 }
