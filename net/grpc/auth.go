470a2f425d5386cca3a52144c2c39832351d1c2f
Michal Cizmazia <micizma@gmail.com>
2019-07-26 14:56:57 +0200

    grpc auth interceptors

---
+++ net/grpc/auth.go
@@ -0,0 +1,48 @@
+package grpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+
+	"github.com/go-ocf/kit/security/jwt"
+)
+
+type AuthInterceptors struct {
+	authFunc grpc_auth.AuthFunc
+}
+
+func MakeAuthInterceptors(authFunc grpc_auth.AuthFunc) AuthInterceptors {
+	return AuthInterceptors{authFunc: authFunc}
+}
+
+func MakeJWTInterceptors(jwksUrl string, claims ClaimsFunc) AuthInterceptors {
+	return MakeAuthInterceptors(ValidateJWT(jwksUrl, claims))
+}
+
+func (f AuthInterceptors) UnaryServer() grpc.UnaryServerInterceptor {
+	return grpc_auth.UnaryServerInterceptor(f.authFunc)
+}
+func (f AuthInterceptors) StreamServer() grpc.StreamServerInterceptor {
+	return grpc_auth.StreamServerInterceptor(f.authFunc)
+}
+
+type ClaimsFunc = func(context.Context) jwt.Claims
+
+func ValidateJWT(jwksUrl string, claims ClaimsFunc) grpc_auth.AuthFunc {
+	validator := jwt.NewValidator(jwksUrl)
+	return func(ctx context.Context) (context.Context, error) {
+		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
+		if err != nil {
+			return nil, err
+		}
+		err = validator.ParseWithClaims(token, claims(ctx))
+		if err != nil {
+			return nil, grpc.Errorf(codes.Unauthenticated, "invalid token: %v", err)
+		}
+		return ctx, nil
+	}
+}
