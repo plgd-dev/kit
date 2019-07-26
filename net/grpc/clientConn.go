1a0d122617221ac5b823dea0f02cdb05a3fdc22a
Michal Cizmazia <micizma@gmail.com>
2019-07-26 16:23:11 +0200

    Intercept Tokens
    - Changed func NewServer to take grpc server options, so that it may
    configured with the interceptors

--- net/grpc/clientConn.go
+++ net/grpc/clientConn.go
@@ -1,7 +1,6 @@
 package grpc
 
 import (
-	"crypto/tls"
 	"fmt"
 
 	"github.com/go-ocf/kit/security"
@@ -10,74 +9,6 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
-
-type clientConnOptions struct {
-	tlsConfig          *tls.Config
-	secure             bool
-	insecure           bool
-	insecureSkipVerify bool
-}
-
-// ClientConnOption configures how we set up the connection.
-type ClientConnOption interface {
-	applyOnClientConn(*clientConnOptions)
-}
-
-// Option configures how we set up the server or the connection.
-type Option interface {
-	ClientConnOption
-	ServerOption
-}
-
-type withTLSOption struct {
-	tlsConfig *tls.Config
-}
-
-func (o withTLSOption) applyOnClientConn(opts *clientConnOptions) {
-	opts.tlsConfig = o.tlsConfig
-	opts.secure = true
-}
-
-// WithTLS creates connection with TLS.
-func WithTLS(tlsConfig *tls.Config) Option {
-	return &withTLSOption{tlsConfig}
-}
-
-type withInsecureConfigOption struct {
-}
-
-func (o withInsecureConfigOption) applyOnClientConn(opts *clientConnOptions) {
-	opts.insecure = true
-}
-
-// WithInsecure returns a NewClientConn which disables transport security for this ClientConn. Note that transport security is required unless WithInsecure is set.
-func WithInsecure() Option {
-	return &withInsecureConfigOption{}
-}
-
-// NewClientConnWithOptions creates grpc client connection. One of WithTLS, WithInsecure must be set.
-func NewClientConnWithOptions(host string, opts ...ClientConnOption) (conn *grpc.ClientConn, err error) {
-	var cfg clientConnOptions
-	for _, o := range opts {
-		o.applyOnClientConn(&cfg)
-	}
-	if !cfg.secure && !cfg.insecure {
-		return nil, fmt.Errorf("cannot use transport layer: not set - use WithTLS or WithInsecure option")
-	}
-	if cfg.secure && cfg.insecure {
-		return nil, fmt.Errorf("cannot use transport layer: both WithTLS and WithInsecure are set")
-	}
-
-	if cfg.secure {
-		conn, err = grpc.Dial(host, grpc.WithTransportCredentials(credentials.NewTLS(cfg.tlsConfig)))
-	} else {
-		conn, err = grpc.Dial(host, grpc.WithInsecure())
-	}
-	if err != nil {
-		return nil, fmt.Errorf("cannot create grpc connection: %v", err)
-	}
-	return
-}
 
 // NewClientConn creates gRPC client connection
 // based on the generated flag security.IsInsecure().
@@ -89,18 +20,18 @@ func NewClientConn(host string, tls *security.TLSConfig) (*grpc.ClientConn, erro
 		return nil, status.Errorf(codes.InvalidArgument, "missing TLS config")
 	}
 
-	var opt Option
+	var opts []grpc.DialOption
 	if security.IsInsecure() {
-		opt = WithInsecure()
+		opts = append(opts, grpc.WithInsecure())
 	} else {
 		tlsCfg, err := security.NewTLSConfigFromConfiguration(*tls, security.VerifyServerCertificate)
 		if err != nil {
 			return nil, status.Errorf(codes.InvalidArgument, "invalid TLS config: %v", err)
 		}
-		opt = WithTLS(tlsCfg)
+		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(tlsCfg)))
 	}
 
-	conn, err := NewClientConnWithOptions(host, opt)
+	conn, err := grpc.Dial(host, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("could not create gRPC client: %v", err)
 	}
