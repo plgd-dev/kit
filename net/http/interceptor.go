a7bd8ddb830e4cea41d195b22b025307c076b3dc
Jozef Kralik <jojo.lwin@gmail.com>
2019-11-27 10:29:22 +0100

    add NewInterceptor to validate scope against method and uri

--- net/http/interceptor.go
+++ net/http/interceptor.go
@@ -2,6 +2,36 @@ package http
 
 import (
 	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/go-ocf/kit/security/jwt"
 )
 
 type Interceptor = func(ctx context.Context, method, uri string) (context.Context, error)
+type AuthArgs struct{ Scope string }
+
+func NewInterceptor(jwksUrl string, tls tls.Config, auths map[string]map[string]AuthArgs) Interceptor {
+	return ValidateJWT(jwksUrl, tls, MakeClaimsFunc(auths))
+}
+
+func MakeClaimsFunc(methods map[string]map[string]AuthArgs) ClaimsFunc {
+	return func(ctx context.Context, method, uri string) Claims {
+		uris, ok := methods[method]
+		if !ok {
+			return &DeniedClaims{fmt.Errorf("inaccessible method: %v", method)}
+		}
+		arg, ok := uris[uri]
+		if !ok {
+			return &DeniedClaims{fmt.Errorf("inaccessible uri: %v %v", method, uri)}
+		}
+		return jwt.NewScopeClaims(arg.Scope)
+	}
+}
+
+type DeniedClaims struct {
+	Err error
+}
+
+func (c DeniedClaims) Valid() error {
+	return c.Err
+}
