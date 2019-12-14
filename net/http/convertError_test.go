f42ca1346f3fab84d7551ab10785e0c2f2371249
Michal Cizmazia <micizma@gmail.com>
2019-12-14 11:26:44 +0100

    parse X509

--- net/http/convertError_test.go
+++ net/http/convertError_test.go
@@ -2,13 +2,13 @@ package http
 
 import (
 	"fmt"
-	"net/http"
+	gocoap "github.com/go-ocf/go-coap"
+	coapCodes "github.com/go-ocf/go-coap/codes"
+	coapStatus "github.com/go-ocf/kit/net/coap/status"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"github.com/stretchr/testify/assert"
-	coapStatus "github.com/go-ocf/kit/net/coap/status"
-	coapCodes "github.com/go-ocf/go-coap/codes"
-	gocoap "github.com/go-ocf/go-coap"
+	"net/http"
 	"testing"
 )
 
@@ -21,20 +21,20 @@ func TestErrToStatus(t *testing.T) {
 		args args
 		want int
 	}{
-		{name:"ol", args: args{err: nil}, want: http.StatusOK},
-		{name:"grpc", args: args{err: status.Error(codes.PermissionDenied, "grpc error")}, want: http.StatusForbidden},
+		{name: "ol", args: args{err: nil}, want: http.StatusOK},
+		{name: "grpc", args: args{err: status.Error(codes.PermissionDenied, "grpc error")}, want: http.StatusForbidden},
 		{
-			name:"coap",
+			name: "coap",
 			args: args{
 				err: coapStatus.Error(gocoap.NewTcpMessage(gocoap.MessageParams{Code: coapCodes.Forbidden}), fmt.Errorf("coap error")),
 			},
 			want: http.StatusForbidden,
 		},
-		{name:"grpc", args: args{err: fmt.Errorf("unknown error")}, want: http.StatusInternalServerError},
+		{name: "grpc", args: args{err: fmt.Errorf("unknown error")}, want: http.StatusInternalServerError},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := ErrToStatus(tt.args.err);
+			got := ErrToStatus(tt.args.err)
 			assert.Equal(t, tt.want, got)
 		})
 	}
