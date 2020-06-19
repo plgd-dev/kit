882bda1490b6b6620ff32328566855e9632f29ae
Jozef Kralik <jozef.kralik@kistler.com>
2020-06-19 09:25:16 +0200

    add sync map

---
+++ sync/map.go
@@ -0,0 +1,91 @@
+package sync
+
+import "sync"
+
+// Map is like a Go map[interface{}]interface{} but is safe for concurrent use by multiple goroutines.
+type Map struct {
+	mutex sync.Mutex
+	data  map[interface{}]interface{}
+}
+
+// NewMap creates map.
+func NewMap() *Map {
+	return &Map{
+		data: make(map[interface{}]interface{}),
+	}
+}
+
+// Delete deletes the value for a key.
+func (m *Map) Delete(key interface{}) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	delete(m.data, key)
+}
+
+// Load returns the value stored in the map for a key, or nil if no value is present.
+// The ok result indicates whether value was found in the map.
+func (m *Map) Load(key interface{}) (value interface{}, ok bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	value, ok = m.data[key]
+	return
+}
+
+// LoadOrStore returns the existing value for the key if present.
+// Otherwise, it stores and returns the given value. The loaded result is true if the value was loaded, false if stored.
+func (m *Map) LoadOrStore(key, value interface{}) (actual interface{}, loaded bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	v, ok := m.data[key]
+	if ok {
+		return v, ok
+	}
+	m.data[key] = value
+	return value, ok
+}
+
+// Replace replaces the existing value with a new value and returns old value for the key.
+func (m *Map) Replace(key, value interface{}) (oldValue interface{}, oldLoaded bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	v, ok := m.data[key]
+	m.data[key] = value
+	return v, ok
+}
+
+// Range calls f sequentially for each key and value present in the map. If f returns false, range stops the iteration.
+func (m *Map) Range(f func(key, value interface{}) bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	for key, value := range m.data {
+		ok := f(key, value)
+		if !ok {
+			return
+		}
+	}
+}
+
+// Store sets the value for a key.
+func (m *Map) Store(key, value interface{}) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.data[key] = value
+}
+
+// PullOut loads and deletes the value for a key.
+func (m *Map) PullOut(key interface{}) (value interface{}, ok bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	value, ok = m.data[key]
+	delete(m.data, key)
+	return
+}
+
+// PullOutAll extracts internal map data and replace it with empty map.
+func (m *Map) PullOutAll() (data map[interface{}]interface{}) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	data = m.data
+	m.data = make(map[interface{}]interface{})
+	return
+}
